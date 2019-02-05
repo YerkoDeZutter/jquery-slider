aac5b8081aee9e3317e8f5488ca10c122b63cf33
Yerko <yerkodezutter@gmail.com>
2019-02-05 11:28:03 +0100

    jquery slider ... done?

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let imgCount = 1;
+
+let imgChange = "";
+
+let reset = false;
+
+function nextImg() {
+
+  if (imgCount <= 5) {
+    $("#slider").css("background-image", 'url("img/slider-' + imgCount + '.jpg")');
+    imgCount++
+  } else {
+    $("#slider").css("background-image", 'url("img/slider-' + imgCount + '.jpg")');
+    imgCount = 0;
+  }
+
+  console.log(imgCount);
+
+  if (reset) {
+    clearTimeout(timeoutHandle)
+    reset = false;
+  } else {
+    var timeoutHandle = setTimeout(nextImg, 5000);
+  }
+}
+
+$("#right").click(function() {
+  reset = true;
+  nextImg()
+});
+
+nextImg()
